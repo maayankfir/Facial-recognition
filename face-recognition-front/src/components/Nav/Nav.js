00cc1fae67529d1e0657c8acda02cea377eaa897
github username <github email address>
2019-02-07 12:39:40 -0500

    logo

---
+++ face-recognition-front/src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Nav = () => {
+
+  return (
+    <nav style= {{display: 'flex', justifyContent: 'flex-end'}}>
+      <p className='f3 link dim black underline pa3 pointer'> Sing Out </p>
+      </nav>
+  )
+
+}
+
+export default Nav;
